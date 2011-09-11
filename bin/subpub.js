6c9933a5bda868e347f86f85b76044bb8a1513fa
Caleb Crane <github@simulacre.org>
2011-09-11 22:55:50 +0900

    local config files will be respected

--- bin/subpub.js
+++ bin/subpub.js
@@ -4,4 +4,18 @@ require.paths.push(__dirname + '/../deps');
 require.paths.push(__dirname + '/../lib');
 process.chdir(__dirname);
 
-require("subpub.js").listen();
+var config = {
+  'port': 6379,
+  'host':"0.0.0.0"
+};
+var Path   = require("path");
+var path   = Path.resolve(process.env.HOME + "/.subpub/config.json");
+
+if (Path.existsSync(path)) {
+  cnf = JSON.parse(require("fs").readFileSync(path));
+  for (var key in cnf) {
+    cnf.hasOwnProperty(key) && (config[key] = cnf[key]);
+  }
+};
+
+require("subpub.js").listen(config["port"], config["host"]);
